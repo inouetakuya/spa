b654d49dcfd606d83d756bf9d599d55a593b5a91
INOUE Takuya <inouetakuya5@gmail.com>
2015-10-19 22:49:51 +0900

    reformat 3.4.4

--- ch03/3.4.4/js/spa.shell.js
+++ ch03/3.4.4/js/spa.shell.js
@@ -1,35 +1,35 @@
 /*
  * spa.shell.js
  * Shell module for SPA
-*/
+ */
 
 /*jslint         browser : true, continue : true,
-  devel  : true, indent  : 2,    maxerr   : 50,
-  newcap : true, nomen   : true, plusplus : true,
-  regexp : true, sloppy  : true, vars     : false,
-  white  : true
-*/
+ devel  : true, indent  : 2,    maxerr   : 50,
+ newcap : true, nomen   : true, plusplus : true,
+ regexp : true, sloppy  : true, vars     : false,
+ white  : true
+ */
 /*global $, spa */
 
 spa.shell = (function () {
   //---------------- BEGIN MODULE SCOPE VARIABLES --------------
   var
     configMap = {
-      main_html : String()
-        + '<div class="spa-shell-head">'
-          + '<div class="spa-shell-head-logo"></div>'
-          + '<div class="spa-shell-head-acct"></div>'
-          + '<div class="spa-shell-head-search"></div>'
-        + '</div>'
-        + '<div class="spa-shell-main">'
-          + '<div class="spa-shell-main-nav"></div>'
-          + '<div class="spa-shell-main-content"></div>'
-        + '</div>'
-        + '<div class="spa-shell-foot"></div>'
-        + '<div class="spa-shell-chat"></div>'
-        + '<div class="spa-shell-modal"></div>'
+      main_html: String()
+      + '<div class="spa-shell-head">'
+      + '<div class="spa-shell-head-logo"></div>'
+      + '<div class="spa-shell-head-acct"></div>'
+      + '<div class="spa-shell-head-search"></div>'
+      + '</div>'
+      + '<div class="spa-shell-main">'
+      + '<div class="spa-shell-main-nav"></div>'
+      + '<div class="spa-shell-main-content"></div>'
+      + '</div>'
+      + '<div class="spa-shell-foot"></div>'
+      + '<div class="spa-shell-chat"></div>'
+      + '<div class="spa-shell-modal"></div>'
     },
-    stateMap  = { $container : null },
+    stateMap = { $container: null },
     jqueryMap = {},
 
     setJqueryMap, initModule;
@@ -42,7 +42,7 @@ spa.shell = (function () {
   // Begin DOM method /setJqueryMap/
   setJqueryMap = function () {
     var $container = stateMap.$container;
-    jqueryMap = { $container : $container };
+    jqueryMap = { $container: $container };
   };
   // End DOM method /setJqueryMap/
   //--------------------- END DOM METHODS ----------------------
@@ -52,13 +52,13 @@ spa.shell = (function () {
 
   //------------------- BEGIN PUBLIC METHODS -------------------
   // Begin Public method /initModule/
-  initModule = function ( $container ) {
+  initModule = function ($container) {
     stateMap.$container = $container;
-    $container.html( configMap.main_html );
+    $container.html(configMap.main_html);
     setJqueryMap();
   };
   // End PUBLIC method /initModule/
 
-  return { initModule : initModule };
+  return { initModule: initModule };
   //------------------- END PUBLIC METHODS ---------------------
 }());
