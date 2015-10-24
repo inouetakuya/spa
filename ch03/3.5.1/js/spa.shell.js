294f669d579779147c6695fa1ad476e6aaf9e1aa
INOUE Takuya <inouetakuya5@gmail.com>
2015-10-24 18:03:35 +0900

    reformat 3.5.1

--- ch03/3.5.1/js/spa.shell.js
+++ ch03/3.5.1/js/spa.shell.js
@@ -1,39 +1,39 @@
 /*
  * spa.shell.js
  * Shell module for SPA
-*/
+ */
 
 /*jslint         browser : true, continue : true,
-  devel  : true, indent  : 2,    maxerr   : 50,
-  newcap : true, nomen   : true, plusplus : true,
-  regexp : true, sloppy  : true, vars     : false,
-  white  : true
-*/
+ devel  : true, indent  : 2,    maxerr   : 50,
+ newcap : true, nomen   : true, plusplus : true,
+ regexp : true, sloppy  : true, vars     : false,
+ white  : true
+ */
 /*global $, spa */
 
 spa.shell = (function () {
   //---------------- BEGIN MODULE SCOPE VARIABLES --------------
   var
     configMap = {
-      main_html : String()
-        + '<div class="spa-shell-head">'
-          + '<div class="spa-shell-head-logo"></div>'
-          + '<div class="spa-shell-head-acct"></div>'
-          + '<div class="spa-shell-head-search"></div>'
-        + '</div>'
-        + '<div class="spa-shell-main">'
-          + '<div class="spa-shell-main-nav"></div>'
-          + '<div class="spa-shell-main-content"></div>'
-        + '</div>'
-        + '<div class="spa-shell-foot"></div>'
-        + '<div class="spa-shell-chat"></div>'
-        + '<div class="spa-shell-modal"></div>',
-      chat_extend_time     : 1000,
-      chat_retract_time    : 300,
-      chat_extend_height   : 450,
-      chat_retract_height  : 15
+      main_html: String()
+      + '<div class="spa-shell-head">'
+      + '<div class="spa-shell-head-logo"></div>'
+      + '<div class="spa-shell-head-acct"></div>'
+      + '<div class="spa-shell-head-search"></div>'
+      + '</div>'
+      + '<div class="spa-shell-main">'
+      + '<div class="spa-shell-main-nav"></div>'
+      + '<div class="spa-shell-main-content"></div>'
+      + '</div>'
+      + '<div class="spa-shell-foot"></div>'
+      + '<div class="spa-shell-chat"></div>'
+      + '<div class="spa-shell-modal"></div>',
+      chat_extend_time: 1000,
+      chat_retract_time: 300,
+      chat_extend_height: 450,
+      chat_retract_height: 15
     },
-    stateMap  = { $container : null },
+    stateMap = { $container: null },
     jqueryMap = {},
 
     setJqueryMap, toggleChat, initModule;
@@ -48,8 +48,8 @@ spa.shell = (function () {
     var $container = stateMap.$container;
 
     jqueryMap = {
-      $container : $container,
-      $chat      : $container.find( '.spa-shell-chat' )
+      $container: $container,
+      $chat: $container.find('.spa-shell-chat')
     };
   };
   // End DOM method /setJqueryMap/
@@ -66,23 +66,27 @@ spa.shell = (function () {
   //   * true  - slider animation activated
   //   * false - slider animation not activated
   //
-  toggleChat = function ( do_extend, callback ) {
+  toggleChat = function (do_extend, callback) {
     var
       px_chat_ht = jqueryMap.$chat.height(),
-      is_open    = px_chat_ht === configMap.chat_extend_height,
-      is_closed  = px_chat_ht === configMap.chat_retract_height,
-      is_sliding = ! is_open && ! is_closed;
+      is_open = px_chat_ht === configMap.chat_extend_height,
+      is_closed = px_chat_ht === configMap.chat_retract_height,
+      is_sliding = !is_open && !is_closed;
 
     // avoid race condition
-    if ( is_sliding ){ return false; }
+    if (is_sliding) {
+      return false;
+    }
 
     // Begin extend chat slider
-    if ( do_extend ) {
+    if (do_extend) {
       jqueryMap.$chat.animate(
-        { height : configMap.chat_extend_height },
+        { height: configMap.chat_extend_height },
         configMap.chat_extend_time,
         function () {
-          if ( callback ){ callback( jqueryMap.$chat ); }
+          if (callback) {
+            callback(jqueryMap.$chat);
+          }
         }
       );
       return true;
@@ -91,10 +95,12 @@ spa.shell = (function () {
 
     // Begin retract chat slider
     jqueryMap.$chat.animate(
-      { height : configMap.chat_retract_height },
+      { height: configMap.chat_retract_height },
       configMap.chat_retract_time,
       function () {
-        if ( callback ){ callback( jqueryMap.$chat ); }
+        if (callback) {
+          callback(jqueryMap.$chat);
+        }
       }
     );
     return true;
@@ -108,18 +114,22 @@ spa.shell = (function () {
 
   //------------------- BEGIN PUBLIC METHODS -------------------
   // Begin Public method /initModule/
-  initModule = function ( $container ){
+  initModule = function ($container) {
     // load HTML and map jQuery collections
     stateMap.$container = $container;
-    $container.html( configMap.main_html );
+    $container.html(configMap.main_html);
     setJqueryMap();
 
     // test toggle
-    setTimeout( function () {toggleChat( true ); }, 3000 );
-    setTimeout( function () {toggleChat( false );}, 8000 );
+    setTimeout(function () {
+      toggleChat(true);
+    }, 3000);
+    setTimeout(function () {
+      toggleChat(false);
+    }, 8000);
   };
   // End PUBLIC method /initModule/
 
-  return { initModule : initModule };
+  return { initModule: initModule };
   //------------------- END PUBLIC METHODS ---------------------
 }());
